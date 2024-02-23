c351c9c3d982d4665204459733c3e898790a805a
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-02-23 16:21:00 +0530

    changes

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const app = express();
 const port = 8000;
 const axios = require("axios");
 const cors = require("cors");
+const { v4 } = require("uuid");
+const data = require("./db.json");
 
-const instance_url = "https://rest.fra-02.braze.eu/users/track";
+console.log(data);
+const track_url = "https://rest.fra-02.braze.eu/users/track";
+const alias_url = "https://rest.fra-02.braze.eu/users/alias/new";
+const identify_url = "https://rest.fra-02.braze.eu/users/identify";
 
 // send a api req to user's track endpoint
 const headers = {
@@ -15,6 +20,10 @@ const headers = {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+// data.map((i) => {
+//   console.log(i.email[0]);
+// });
 
 app.get("/", cors(), (req, res) => {
   res.send("Hello World!");
@@ -30,13 +39,77 @@ app.get("/", cors(), (req, res) => {
 
 app.post("/users/track", (req, res) => {
   const data = req.body;
-  axios
-    .post(instance_url, data, {
-      headers: headers,
-    })
-    .then((data) => console.log(data));
+  // console.log(data);
+
+  // something with data
+
+  const alias = {
+    user_aliases: [
+      {
+        alias_name: "email",
+        alias_label: data.email,
+      },
+    ],
+  };
+
+  const userData = {
+    attributes: [
+      {
+        email: data.email,
+        signed_up_email: true,
+      },
+    ],
+    events: [
+      {
+        name: "Signed up for email",
+        time: data.timestamp,
+      },
+    ],
+  };
+
+
+  console.log(userData)
+
+  // axios
+  //   .post(alias_url, data, {
+  //     headers: headers,
+  //   })
+  //   .then((data) => console.log(data));
+
+  // axios
+  //   .post(track_url, data, {
+  //     headers: headers,
+  //   })
+  //   .then((data) => console.log(data));
 
   res.send(201);
+});
+
+app.post("/identify", (req, res) => {
+  const data = req.body;
+
+  const user_id = v4()
+
+  const identify = {
+    aliases_to_identify: [
+      {
+        external_id: user_id,
+        user_alias: {
+          alias_name: "email",
+          alias_label: "kirti@email.com",
+        },
+      },
+    ],
+    merge_behavior: "merge",
+  };
+
+  console.log(identify)
+  // axios
+  //   .post(instance_url, data, {
+  //     headers: headers,
+  //   })
+  //   .then((data) => console.log(data));
+  res.send(identify);
 });
 
 app.listen(port, () => {
---
+++ readme.txt
@@ -0,0 +1,6 @@
+const alias_of_emails = {
+  "kirti@email.com": "9820042672",
+};
+
+module.exports = alias_of_emails;
+// export default alias_of_emails;
